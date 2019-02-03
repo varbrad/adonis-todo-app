92a6a62e3ef4ca99d4a3fbabfaa97fb8d6ce3edb
Brad Davies <varbrad@users.noreply.github.com>
2019-02-03 18:26:20 +0000

    Initial setup of stuff

---
+++ config/app.js
@@ -0,0 +1,48 @@
+const Env = use('Env')
+
+const APP_NAME = Env.getOrFail('APP_NAME')
+
+module.exports = {
+  name: APP_NAME,
+  appKey: Env.getOrFail('APP_KEY'),
+  http: {
+    allowMethodSpoofing: true,
+    trustProxy: false,
+    subdomainOffset: 2,
+    jsonpCallback: 'callback',
+    etag: false,
+  },
+  views: {
+    cache: Env.get('CACHE_VIEWS', true),
+  },
+  static: {
+    dotfiles: 'ignore',
+    etag: true,
+    extensions: false,
+  },
+  locales: {
+    loader: 'file',
+    locale: 'en',
+  },
+  logger: {
+    transport: 'console',
+    console: {
+      driver: 'console',
+      name: APP_NAME,
+      level: 'info',
+    },
+
+    file: {
+      driver: 'file',
+      name: APP_NAME,
+      filename: `${APP_NAME}.log`,
+      level: 'info',
+    },
+  },
+  cookie: {
+    httpOnly: true,
+    sameSite: false,
+    path: '/',
+    maxAge: 7200,
+  },
+}
